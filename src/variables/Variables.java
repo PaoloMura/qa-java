e6217812f508806ee8289d4ce258ed9031f0bebf
PaoloMura <paolo@muraonline.com>
2023-07-21 14:42:18 +0100

    refactored to use methods

--- src/variables/Variables.java
+++ src/variables/Variables.java
@@ -3,12 +3,14 @@ package variables;
 import java.util.Scanner;
 
 public class Variables {
-    public static void main(String[] args) {
-//        String firstName = "Paolo";
-//        String lastName = "Mura";
-//        String name = firstName + lastName;
-//        System.out.println(name);
+    private static void firstTask() {
+        String firstName = "Paolo";
+        String lastName = "Mura";
+        String name = firstName + lastName;
+        System.out.println(name);
+    }
 
+    private static void secondTask() {
         int num1 = 10;
         int num2 = 2;
         System.out.println(num1 + num2);
@@ -20,14 +22,26 @@ public class Variables {
         System.out.println(num3++);
         System.out.println(num3);
         System.out.println(++num3);
+    }
 
+    private static void thirdTask() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your first name:");
         String firstName = scanner.nextLine();
+        System.out.println("Enter your second name:");
         String lastName = scanner.nextLine();
         System.out.println("Hello " + firstName + " " + lastName);
 
-        num1 = scanner.nextInt();
-        num2 = scanner.nextInt();
+        System.out.println("Enter your first number:");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter your second number:");
+        int num2 = scanner.nextInt();
         System.out.println(num1 + num2);
+    }
+
+    public static void main(String[] args) {
+        firstTask();
+        secondTask();
+        thirdTask();
     }
 }
